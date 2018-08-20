37ecc8ac5e78d14d6efaa39a94f6e2991f69a42f
James Reichard <jpqreichard@gmail.com>
2018-08-19 20:35:49 -0400

    Update comments

--- src/VimeoPlayer/VimeoPlayer.js
+++ src/VimeoPlayer/VimeoPlayer.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 import ReactPlayer from 'react-player'
-import { PlaybackControls, MuteToggleButton } from 'react-player-controls'
 import PlayerControls from '../PlayerControls/PlayerControls'
 import './VimeoPlayer.css'
 
 // TODO: Add image type functionality
-// TODO: div.masky should be a Controls component
+// TODO: should div.masky be in PlayerControls?
 // TODO: slideshow functionality
+//  onEnd go to next video
 // TODO: customize loading screen with onStart
 //   Use [monster?] images for loading in between videos
 // TODO: change dimensions of player on window resize
@@ -20,7 +20,7 @@ export default class VimeoPlayer extends Component {
       height: null,
       aspectRatio: null
     },
-    aspectRatio: window.innerWidth / window.innerHeight,
+    // aspectRatio: window.innerWidth / window.innerHeight,
     volume: 0,
     playing: true
   }
@@ -85,7 +85,7 @@ export default class VimeoPlayer extends Component {
     const containerAspectRatio = window.innerWidth / window.innerHeight
     if (containerAspectRatio > this.state.video.aspectRatio) {
       return {
-        width: this.state.video.height / this.state.video.aspectRatio,
+        width: this.state.video.height * this.state.video.aspectRatio,
         // wrong operator?
         // instead of this.state.video.height, need to adjust this proportionally, I think
         height: window.innerHeight
@@ -93,7 +93,7 @@ export default class VimeoPlayer extends Component {
     } else { // they could be equal
       return {
         width: window.innerWidth,
-        height: this.state.video.width * this.state.video.aspectRatio
+        height: this.state.video.width / this.state.video.aspectRatio
         // wrong operator?
         // instead of this.state.video.width, need to adjust this proportionally, I think
       }
@@ -143,7 +143,6 @@ export default class VimeoPlayer extends Component {
           loop
           onBuffer={_ => console.log('Desire is the root of all buffering', _)}
         />
-
       </div>
     )
   }
