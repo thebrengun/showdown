26f1cb8848b39c1ba5bd4641441d7296b2a8d6ac
James Reichard <jpqreichard@gmail.com>
2018-08-19 20:35:57 -0400

    Add dummy data

---
+++ src/dummyData.js
@@ -0,0 +1,37 @@
+export default {
+  data: [
+    {
+      title: 'SILVER SOUND SHOWDOWN CALL FOR ENTRIES',
+      type: 'vimeo_video',
+      vimeo_id: '110497969',
+      width: '1280',
+      height: '720',
+      vimeo_image: 'http://i.vimeocdn.com/video/494780643_640.jpg'
+    },
+    {
+      title: 'download_1',
+      type: 'image',
+      url: 'http://showdown.silversound.us/wp-content/uploads/sites/3/2018/07/download_1.jpg',
+      caption: '',
+      width: 664,
+      height: 664
+    },
+    {
+      title: 'Flying Cat - Haagen dazs',
+      type: 'vimeo_video',
+      vimeo_id: '126494659',
+      width: '640',
+      height: '360',
+      vimeo_image: 'http://i.vimeocdn.com/video/516955213_640.jpg'
+    },
+    {
+      title: 'Vessels - Deb Oh & the Cavaliers',
+      type: 'vimeo_video',
+      vimeo_id: '85876630',
+      width: '1280',
+      height: '720',
+      vimeo_image: 'http://i.vimeocdn.com/video/713714517_640.jpg'
+    }
+  ],
+  success: 'true'
+}
