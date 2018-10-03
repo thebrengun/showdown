5b6c155c8d9399a5f6ad02f7ef5444887890f13d
James Reichard <jpqreichard@gmail.com>
2018-10-03 16:54:09 -0400

    Scope CSS to react app-root and remove body resets.

--- src/CoverImage/CoverImage.js
+++ src/CoverImage/CoverImage.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import './CoverImage.css'
 
-// import guitar from './images/guitar.png'
-// import harmonica from './images/harmonica.png'
-// import keyboard from './images/keyboard.png'
-// import sax from './images/sax.png'
+import guitar from './images/guitar.png'
+import harmonica from './images/harmonica.png'
+import keyboard from './images/keyboard.png'
+import sax from './images/sax.png'
 
-const guitar = '/wp-content/themes/showdown2018/static/media/keyboard.81baa7e6.png'
-const harmonica = '/wp-content/themes/showdown2018/static/media/harmonica.4eaaae7c.png'
-const keyboard = '/wp-content/themes/showdown2018/static/media/guitar.d351fcb4.png'
-const sax = '/wp-content/themes/showdown2018/static/media/sax.5d5f335c.png'
+// const guitar = '/wp-content/themes/showdown2018/static/media/keyboard.81baa7e6.png'
+// const harmonica = '/wp-content/themes/showdown2018/static/media/harmonica.4eaaae7c.png'
+// const keyboard = '/wp-content/themes/showdown2018/static/media/guitar.d351fcb4.png'
+// const sax = '/wp-content/themes/showdown2018/static/media/sax.5d5f335c.png'
 
-export default ({url}) => {
+export default ({ url }) => {
   const images = [guitar, harmonica, keyboard, sax]
   const randomImage = () => images[parseInt(Math.random() * images.length, 10)]
   console.log(randomImage())
